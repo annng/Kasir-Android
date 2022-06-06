8947a6fb97c3d5f2255df92a0da50f4c019ec896
ArifGlory <arifglory46@gmail.com>
2022-06-06 14:42:47 +0700

    add fitur hapus barang

--- app/src/main/java/com/tapisdev/penjualankasir/adapter/AdapterBarang.kt
+++ app/src/main/java/com/tapisdev/penjualankasir/adapter/AdapterBarang.kt
@@ -1,19 +1,17 @@
 package com.tapisdev.penjualankasir.adapter
 
-import android.content.Context
+
 import android.content.Intent
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.tapisdev.penjualankasir.databinding.ItemsBarangBinding
 import com.tapisdev.penjualankasir.model.Barang
-import es.dmoral.toasty.Toasty
-import java.io.Serializable
+import com.tapisdev.penjualankasir.BuildConfig
+import com.tapisdev.penjualankasir.activity.DetailBarangActivity
 
 class AdapterBarang(private val list:ArrayList<Barang>) : RecyclerView.Adapter<AdapterBarang.MyViewHolder>(){
 
@@ -37,16 +35,20 @@ class AdapterBarang(private val list:ArrayList<Barang>) : RecyclerView.Adapter<A
         with(holder){
             with(list.get(position)){
 
-                val url = ""
+                val imageUrl = BuildConfig.BASE_URL+"img/barang/"+list?.get(position).picture
+               // Log.d("imgbarang",""+imageUrl)
+                Glide.with(binding.rlBarang.context)
+                    .load(imageUrl)
+                    .into(binding.ivBarang)
 
                 binding.tvNamaBarang.setText(""+list?.get(position).name)
                 binding.tvStok.setText("Sisa Stok "+list?.get(position).stok+" "+list?.get(position).satuan)
                 binding.tvDeskripsi.setText(list?.get(position).deskripsi)
 
                 binding.rlBarang.setOnClickListener {
-                   /* val i  = Intent(binding.rlBarang.context,DetailSuratMasukActivity::class.java)
+                    val i  = Intent(binding.rlBarang.context,DetailBarangActivity::class.java)
                     i.putExtra("barang",list?.get(position))
-                    binding.rlBarang.context.startActivity(i)*/
+                    binding.rlBarang.context.startActivity(i)
                 }
 
             }
