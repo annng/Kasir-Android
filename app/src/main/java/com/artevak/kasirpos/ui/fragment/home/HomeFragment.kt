322fb29db571a166762add1d1d4ac9458ec72e41
anang.pambudi <anang.pambudi@sampingan.co.id>
2022-08-18 14:23:28 +0700

    - show stock & customer in home page

--- app/src/main/java/com/artevak/kasirpos/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/artevak/kasirpos/ui/fragment/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.artevak.kasirpos.R
@@ -16,15 +17,19 @@ import com.artevak.kasirpos.data.model.Barang
 import com.artevak.kasirpos.data.model.DataChartPenjualan
 import com.artevak.kasirpos.data.model.Customer
 import com.artevak.kasirpos.response.firebase.ResponseData
+import com.artevak.kasirpos.response.firebase.StatusRequest
 import com.artevak.kasirpos.ui.activity.customer.add.TambahPelangganActivity
 import com.artevak.kasirpos.ui.activity.profile.view.ProfileActivity
 import com.artevak.kasirpos.ui.adapter.AdapterBarang
 import com.artevak.kasirpos.ui.adapter.AdapterPelanggan
+import com.artevak.kasirpos.ui.fragment.transaction.TransactionViewModel
 import com.facebook.shimmer.ShimmerFrameLayout
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import es.dmoral.toasty.Toasty
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeFragment : BaseFragment() {
 
@@ -51,6 +56,8 @@ class HomeFragment : BaseFragment() {
     var TAG_GET_CHART = "datachart"
     var TAG_GET_MORE_PELANGGAN = "morepelanggan"
     var KATA_KUNCI = ""
+
+    val viewModel: HomeViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -94,10 +101,67 @@ class HomeFragment : BaseFragment() {
             startActivity(i)
         }
 
-        getDataPelanggan()
-        getDataBarang()
+        observeData()
         getDataChart()
+
+        viewModel.getCustomers()
+        viewModel.getItem()
 //        configChartModel()
+    }
+
+    private fun observeData(){
+        viewModel.customers.observe(viewLifecycleOwner){
+            when(it.status){
+                StatusRequest.LOADING ->{
+                    showLoadingShimmerPelanggan()
+                }
+                StatusRequest.SUCCESS -> {
+                    listCustomer.clear()
+                    it.data?.let { it1 -> listCustomer.addAll(it1) }
+                    adapter.notifyDataSetChanged()
+
+                    hideLoadingShimmerPelanggan()
+
+                    if (listCustomer.size == 0) {
+                        binding.tvInfoEmpty.visibility = View.VISIBLE
+                    }
+                }
+                else -> {
+                    it.message?.let { it1 ->
+                        Toasty.error(requireContext(),
+                            it1, Toast.LENGTH_SHORT, true).show()
+                    }
+                    hideLoadingShimmerPelanggan()
+                }
+            }
+        }
+
+        viewModel.items.observe(viewLifecycleOwner){
+            when(it.status){
+                StatusRequest.LOADING -> {
+                    showLoadingShimmerBarang()
+                    resetPagination()
+                }
+                StatusRequest.SUCCESS -> {
+                    listBarang.clear()
+                    it.data?.let { it1 -> listBarang.addAll(it1) }
+                    adapterBarang.notifyDataSetChanged()
+                    hideLoadingShimmerBarang()
+
+                    if (listBarang.size == 0) {
+                        binding.tvInfoEmpty.visibility = View.VISIBLE
+                    }
+
+                }
+                else -> {
+                    it.message?.let { it1 ->
+                        Toasty.error(requireContext(),
+                            it1, Toast.LENGTH_SHORT, true).show()
+                    }
+                    hideLoadingShimmerBarang()
+                }
+            }
+        }
     }
 
     fun configChartModel() {
@@ -147,81 +211,6 @@ class HomeFragment : BaseFragment() {
         NEXT_PAGE = CURRENT_PAGE + 1
     }
 
-    fun getDataPelanggan() {
-        //loading
-//        showLoadingShimmerPelanggan()
-//        resetPagination()
-
-        listCustomer.clear()
-//        listCustomer.add(
-//            Customer(
-//                name = "Anang",
-//                phone = "+6285747325450",
-//                alamat = "Soka, Lerep"
-//            )
-//        )
-//        listCustomer.add(
-//            Customer(
-//                name = "Ardhea",
-//                phone = "+6285747123456",
-//                alamat = "Banyumanik"
-//            )
-//        )
-        adapter.notifyDataSetChanged()
-
-        hideLoadingShimmerPelanggan()
-
-
-
-        if (listCustomer.size == 0) {
-            binding.tvInfoEmpty.visibility = View.VISIBLE
-        }
-
-
-        //when failed
-//        Toasty.error(requireContext(), "gagal mengambil data", Toast.LENGTH_SHORT, true).show()
-        hideLoadingShimmerPelanggan()
-
-    }
-
-    fun getDataBarang() {
-        showLoadingShimmerBarang()
-        resetPagination()
-
-
-
-        listBarang.clear()
-        listBarang.add(
-            ResponseData(
-                Barang(
-                    name = "Samsung S21",
-                    harga_beli = 180000,
-                    harga_jual = 200000,
-                    deskripsi = "Ram 8/256"
-                ), ""
-            )
-        )
-        listBarang.add(
-            ResponseData(
-                Barang(
-                    name = "Macbook Pro 2020",
-                    harga_beli = 210000,
-                    harga_jual = 2500000,
-                    deskripsi = "Mahal boss"
-                ), ""
-            )
-        )
-
-        adapterBarang.notifyDataSetChanged()
-
-        hideLoadingShimmerBarang()
-
-        if (listBarang.size == 0) {
-            binding.tvInfoEmpty.visibility = View.VISIBLE
-        }
-
-        hideLoadingShimmerBarang()
-    }
 
     fun getDataChart() {
 
--- app/src/main/java/com/artevak/kasirpos/ui/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/artevak/kasirpos/ui/fragment/home/HomeViewModel.kt
@@ -1,7 +1,27 @@
 package com.artevak.kasirpos.ui.fragment.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.artevak.kasirpos.base.BaseViewModel
+import com.artevak.kasirpos.data.model.Barang
+import com.artevak.kasirpos.data.model.Customer
+import com.artevak.kasirpos.response.firebase.ResponseData
+import com.artevak.kasirpos.response.firebase.ResponseProcess
 
 class HomeViewModel(private val useCase: HomeUseCase) : BaseViewModel() {
 
+    private val _customers = MutableLiveData<ResponseProcess<ArrayList<ResponseData<Customer>>?>>()
+    val customers : LiveData<ResponseProcess<ArrayList<ResponseData<Customer>>?>> = _customers
+
+    private val _items = MutableLiveData<ResponseProcess<ArrayList<ResponseData<Barang>>?>>()
+    val items : LiveData<ResponseProcess<ArrayList<ResponseData<Barang>>?>> = _items
+
+
+    fun getCustomers(){
+        useCase.getCustomers(_customers)
+    }
+
+    fun getItem(){
+        useCase.getItem(_items)
+    }
 }
